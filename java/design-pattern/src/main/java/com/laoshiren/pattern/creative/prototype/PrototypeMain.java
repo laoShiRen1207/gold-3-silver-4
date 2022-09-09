e1796d1131cbf7bd6490b8fbc0b9dc9683cc4b0c
项德华 <15207034473@163.com>
2022-09-09 14:30:09 +0800

    init repo

--- java/design-pattern/src/main/java/com/laoshiren/pattern/creative/prototype/PrototypeMain.java
+++ java/design-pattern/src/main/java/com/laoshiren/pattern/creative/prototype/PrototypeMain.java
@@ -7,4 +7,36 @@ package com.laoshiren.pattern.creative.prototype;
  * @Date 20:14 2022/9/8
  */
 public class PrototypeMain {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Student student = new Student();
+        student.setName("laoshiren");
+
+        Object clone = student.clone();
+
+        System.out.println(student.equals(clone));
+    }
+
+}
+
+
+class Student implements Cloneable  {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        Student student = (Student) super.clone();
+        student.name = new String(name);
+        return student;   //成员拷贝完成后，再返回
+    }
+
 }
